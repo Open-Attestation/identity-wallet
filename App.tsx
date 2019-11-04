c73134b574160d7825ad845b1d8a398beb953ca6
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-04 17:24:58 +0800

    Added sample document renderer

--- App.tsx
+++ App.tsx
@@ -2,12 +2,23 @@ import React from "react";
 import Storybook from "./storybook";
 import { IS_STORYBOOK_VIEW } from "./src/config";
 import { StyleSheet, Text, View } from "react-native";
+import { WebView } from 'react-native-webview';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app!</Text>
+    <WebView
+      // originWhitelist={["*"]}
+      source={{ uri: "https://google.com/" }}
+      style={{ flex: 1, width: "100%" }}
+      // javaScriptEnabled={true}
+      // injectedJavaScript={`
+      //   var documentToRender = ${JSON.stringify(data)};
+      //   window.openAttestation.renderDocument(documentToRender);
+      // `}
+    />
+    // </View>
   );
 };
 
