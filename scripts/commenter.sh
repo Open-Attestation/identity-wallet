33b5ef81db4a33d2333b13690c07f6a82c90d28e
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-06 13:35:25 +0800

    fix: storybook link

--- scripts/commenter.sh
+++ scripts/commenter.sh
@@ -6,7 +6,7 @@ EXPO_PROJECT="@dlt/identitywallet"
 PUBLISH_TEXT="Published to https://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
 STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/${EXPO_PROJECT}?release-channel=storybook-${PULL_REQUEST_ID}"
 QR_CODE="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
-QR_CODE_STORYBOOK="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
+QR_CODE_STORYBOOK="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/${EXPO_PROJECT}?release-channel=storybook-${PULL_REQUEST_ID}"
 
 curl -X POST \
   https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/${GITHUB_PROJECT}/issues/${PULL_REQUEST_ID}/comments \
