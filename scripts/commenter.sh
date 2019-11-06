b3269f984bb5e35da8d3337c1eb7b46fe150079f
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-06 13:29:18 +0800

    fix: qrcode

--- scripts/commenter.sh
+++ scripts/commenter.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 PULL_REQUEST_ID=$(echo $CI_PULL_REQUEST | cut -c 58-)
-PUBLISH_TEXT="Published to https://exp.host/@dlt/identitywallet?release-channel=${PULL_REQUEST_ID}"
-STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/@dlt/identitywallet?release-channel=storybook-${PULL_REQUEST_ID}"
-QR_CODE="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/@workpasssg/sg-workpass?release-channel=${PULL_REQUEST_ID}"
+GITHUB_PROJECT="Open-Attestation/identity-wallet"
+EXPO_PROJECT="@dlt/identitywallet"
+
+PUBLISH_TEXT="Published to https://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
+STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/${EXPO_PROJECT}?release-channel=storybook-${PULL_REQUEST_ID}"
+QR_CODE="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
+QR_CODE_STORYBOOK="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/${EXPO_PROJECT}?release-channel=${PULL_REQUEST_ID}"
 
 curl -X POST \
-  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/Open-Attestation/identity-wallet/issues/${PULL_REQUEST_ID}/comments \
+  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/${GITHUB_PROJECT}/issues/${PULL_REQUEST_ID}/comments \
   -H 'Content-Type: application/json' \
-  -d '{ "body": "![Expo QR]('"$QR_CODE"')\n'"$STORYBOOK_PUBLISH_TEXT"'" }'
+  -d '{ "body": "![Expo QR]('"$QR_CODE_STORYBOOK"')\n'"$STORYBOOK_PUBLISH_TEXT"'" }'
 
 curl -X POST \
-  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/Open-Attestation/identity-wallet/issues/${PULL_REQUEST_ID}/comments \
+  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/${GITHUB_PROJECT}/issues/${PULL_REQUEST_ID}/comments \
   -H 'Content-Type: application/json' \
   -d '{ "body": "![Expo QR]('"$QR_CODE"')\n'"$PUBLISH_TEXT"'" }'
