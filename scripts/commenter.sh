bcac5c112b24864ff7dee9c554db0718791f41c6
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-06 13:14:01 +0800

    fix: commenter target

--- scripts/commenter.sh
+++ scripts/commenter.sh
@@ -5,11 +5,11 @@ STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/@workpasssg/sg-workpass?re
 QR_CODE="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/@workpasssg/sg-workpass?release-channel=${PULL_REQUEST_ID}"
 
 curl -X POST \
-  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/sgworkpass/mobile/issues/${PULL_REQUEST_ID}/comments \
+  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/Open-Attestation/identity-wallet/issues/${PULL_REQUEST_ID}/comments \
   -H 'Content-Type: application/json' \
   -d '{ "body": "![Expo QR]('"$QR_CODE"')\n'"$STORYBOOK_PUBLISH_TEXT"'" }'
 
 curl -X POST \
-  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/sgworkpass/mobile/issues/${PULL_REQUEST_ID}/comments \
+  https://${GITHUB_ACCESS_TOKEN_COMMENTER}@api.github.com/repos/Open-Attestation/identity-wallet/issues/${PULL_REQUEST_ID}/comments \
   -H 'Content-Type: application/json' \
   -d '{ "body": "![Expo QR]('"$QR_CODE"')\n'"$PUBLISH_TEXT"'" }'
