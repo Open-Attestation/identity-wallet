065581b3c6be5a54cfca4045cbd377726b759811
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-06 13:15:35 +0800

    fix: publish target

--- scripts/commenter.sh
+++ scripts/commenter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PULL_REQUEST_ID=$(echo $CI_PULL_REQUEST | cut -c 58-)
-PUBLISH_TEXT="Published to https://exp.host/@workpasssg/sg-workpass?release-channel=${PULL_REQUEST_ID}"
-STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/@workpasssg/sg-workpass?release-channel=storybook-${PULL_REQUEST_ID}"
+PUBLISH_TEXT="Published to https://exp.host/@dlt/identitywallet?release-channel=${PULL_REQUEST_ID}"
+STORYBOOK_PUBLISH_TEXT="Published to https://exp.host/@dlt/identitywallet?release-channel=storybook-${PULL_REQUEST_ID}"
 QR_CODE="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=exp://exp.host/@workpasssg/sg-workpass?release-channel=${PULL_REQUEST_ID}"
 
 curl -X POST \
