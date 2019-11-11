0fe9cc9da976246aea91822c633bb1c05fae7c0f
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-11 15:47:02 +0800

    feat: removed ugly header

--- src/navigation/StackNavigator/index.tsx
+++ src/navigation/StackNavigator/index.tsx
@@ -1,10 +1,15 @@
 import { createStackNavigator } from "react-navigation-stack";
 import MainScreen from "./MainScreen";
 
-const StackNavigator = createStackNavigator({
-  MainScreen: {
-    screen: MainScreen
+const StackNavigator = createStackNavigator(
+  {
+    MainScreen: {
+      screen: MainScreen
+    }
+  },
+  {
+    headerMode: "none"
   }
-});
+);
 
 export default StackNavigator;
