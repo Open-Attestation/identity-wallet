9ef79002c96eced1300e25744541c3d97c4d001b
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-14 16:25:43 +0800

    feat: added ebl to document demo

--- storybook/stories/DocumentRenderer/index.js
+++ storybook/stories/DocumentRenderer/index.js
@@ -4,11 +4,16 @@ import { storiesOf } from "@storybook/react-native";
 import { DocumentRenderer } from "../../../src/components/DocumentRenderer/DocumentRenderer";
 import demoOc from "./demo-oc.json";
 import demoCaas from "./demo-caas.json";
+import demoEbl from "./demo-ebl.json";
 
-storiesOf("DocumentRenderer", module).add("Demo OpenCerts", () => (
+storiesOf("DocumentRenderer", module).add("OpenCerts", () => (
   <DocumentRenderer document={demoOc} />
 ));
 
-storiesOf("DocumentRenderer", module).add("Demo CAAS", () => (
+storiesOf("DocumentRenderer", module).add("UALP", () => (
   <DocumentRenderer document={demoCaas} />
+));
+
+storiesOf("DocumentRenderer", module).add("eBL", () => (
+  <DocumentRenderer document={demoEbl} />
 ));
