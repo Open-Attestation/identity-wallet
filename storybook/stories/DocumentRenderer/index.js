f2414cdc77be189764f8bf5f983502405194a93d
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-15 16:51:33 +0800

    chore: moved fixtures around

--- storybook/stories/DocumentRenderer/index.js
+++ storybook/stories/DocumentRenderer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import { storiesOf } from "@storybook/react-native";
 
 import { DocumentRenderer } from "../../../src/components/DocumentRenderer/DocumentRenderer";
-import demoOc from "./demo-oc.json";
-import demoCaas from "./demo-caas.json";
-import demoEbl from "./demo-ebl.json";
+import demoOc from "../../../fixtures/demo-oc.json"
+import demoCaas from "../../../fixtures/demo-caas.json";
+import demoEbl from "../../../fixtures/demo-ebl.json";
 
 const mockBack = () => {
   alert("Back!");
