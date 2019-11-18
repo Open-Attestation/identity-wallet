2ce86575c29e2921e3e78b5ceeb96fac5d3a870b
Raymond Yeh <yehjxraymond@gmail.com>
2019-11-18 12:49:55 +0800

    chore: refactored WebViewFrame

--- src/components/DocumentRenderer/DocumentRenderer.tsx
+++ src/components/DocumentRenderer/DocumentRenderer.tsx
@@ -2,7 +2,6 @@
 import React, { FunctionComponent, useState } from "react";
 import { Document } from "@govtechsg/open-attestation";
 import ReactNative, { View } from "react-native";
-import { WebViewMessageEvent } from "react-native-webview";
 import { Tab } from "./TemplateTabs";
 import { WebViewFrame } from "./WebViewFrame";
 import { DocumentRendererHeader } from "./DocumentRendererHeader";
@@ -21,33 +20,28 @@ export const DocumentRenderer: FunctionComponent<DocumentRenderer> = ({
   document,
   goBack
 }) => {
-  const [template, setTemplate] = useState<Tab[]>([]);
-  const [goToTemplate, setGoToTemplate] = useState();
-  const [activeTabId, setActiveTabId] = useState();
+  const [tabs, setTabs] = useState<Tab[]>([]);
+  const [goToTab, setGoToTab] = useState<(tabId: string) => void>(() => {});
+  const [activeTabId, setActiveTabId] = useState<string>("");
 
   const tabSelect = (id: string): void => {
     setActiveTabId(id);
-    goToTemplate(id);
-  };
-
-  const onTemplateMessageHandler = (event: WebViewMessageEvent): void => {
-    const tabs = JSON.parse(event.nativeEvent.data);
-    setTemplate(tabs);
-    setActiveTabId(tabs[0].id);
+    goToTab(id);
   };
 
   return (
     <View style={wrapperStyle}>
       <DocumentRendererHeader
         goBack={goBack}
-        tabs={template}
+        tabs={tabs}
         tabSelect={tabSelect}
         activeTabId={activeTabId}
       />
       <WebViewFrame
-        setGoToTemplate={setGoToTemplate}
         document={document}
-        onTemplateMessageHandler={onTemplateMessageHandler}
+        setGoToTab={setGoToTab}
+        setActiveTabId={setActiveTabId}
+        setTabs={setTabs}
       />
     </View>
   );
