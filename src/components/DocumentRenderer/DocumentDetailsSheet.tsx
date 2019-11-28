16adae4ee13d41dcc053539a6ca43ab243c8bd18
Sebastian Quek <sebq13@gmail.com>
2019-11-28 16:02:25 +0800

    feat: add ValidityBanner to DocumentDetailsSheet

--- src/components/DocumentRenderer/DocumentDetailsSheet.tsx
+++ src/components/DocumentRenderer/DocumentDetailsSheet.tsx
@@ -1,9 +1,11 @@
-import React, { FunctionComponent, useState } from "react";
+import React, { FunctionComponent, useState, useRef, useEffect } from "react";
 import { LayoutChangeEvent, View, Text } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { BottomSheet } from "../Layout/BottomSheet";
-import { Document } from "@govtechsg/open-attestation";
+import { Document, SignedDocument } from "@govtechsg/open-attestation";
 import QRIcon from "../../../assets/icons/qr.svg";
+import { ValidityBanner, CheckStatus } from "./ValidityBanner";
+import { checkValidity } from "../../services/DocumentVerifier";
 
 interface DocumentDetailsSheet {
   document: Document;
@@ -13,16 +15,41 @@ export const DocumentDetailsSheet: FunctionComponent<DocumentDetailsSheet> = ({
   document
 }) => {
   const [headerHeight, setHeaderHeight] = useState(0);
+  const hasHeaderHeightBeenSet = useRef(false);
   const onHeaderLayout = (event: LayoutChangeEvent): void => {
-    const { height } = event.nativeEvent.layout;
-    setHeaderHeight(height + 56);
+    if (!hasHeaderHeightBeenSet.current) {
+      const { height } = event.nativeEvent.layout;
+      setHeaderHeight(height + 56);
+      hasHeaderHeightBeenSet.current = true;
+    }
   };
 
   const { name } = document.data.issuers[0];
   const issuerName = name.split(":")[2];
 
+  const [tamperedCheck, setTamperedCheck] = useState<CheckStatus>("checking");
+  const [issuedCheck, setIssuedCheck] = useState<CheckStatus>("checking");
+  const [revokedCheck, setRevokedCheck] = useState<CheckStatus>("checking");
+  const [issuerCheck, setIssuerCheck] = useState<CheckStatus>("checking");
+
+  useEffect(() => {
+    const [verifyHashIssuedRevoked, verifyIdentity] = checkValidity(
+      document as SignedDocument
+    );
+
+    verifyHashIssuedRevoked.then(v => {
+      setTamperedCheck(v.hash.checksumMatch ? "valid" : "invalid");
+      setIssuedCheck(v.issued.issuedOnAll ? "valid" : "invalid");
+      setRevokedCheck(v.revoked.revokedOnAny ? "invalid" : "valid");
+    });
+
+    verifyIdentity.then(v => {
+      setIssuerCheck(v.identifiedOnAll ? "valid" : "invalid");
+    });
+  }, [document]);
+
   return (
-    <BottomSheet snapPoints={[headerHeight, "80%"]}>
+    <BottomSheet snapPoints={[headerHeight, "83%"]}>
       {openSheet => (
         <View testID="document-details">
           <View
@@ -31,6 +58,14 @@ export const DocumentDetailsSheet: FunctionComponent<DocumentDetailsSheet> = ({
               paddingBottom: 32
             }}
           >
+            <View style={{ marginHorizontal: -24, marginBottom: 20 }}>
+              <ValidityBanner
+                tamperedCheck={tamperedCheck}
+                issuedCheck={issuedCheck}
+                revokedCheck={revokedCheck}
+                issuerCheck={issuerCheck}
+              />
+            </View>
             <View style={{ flexDirection: "row" }}>
               <View style={{ flex: 1 }}>
                 <Text style={{ fontSize: 12, marginBottom: 8 }}>Issued by</Text>
