4b9db05b919007502fe3b32fa1a72b826d5a31ba
Sebastian Quek <sebq13@gmail.com>
2019-11-28 16:02:44 +0800

    test: add tests for DocumentDetailsSheet

--- src/components/DocumentRenderer/DocumentDetailsSheet.test.tsx
+++ src/components/DocumentRenderer/DocumentDetailsSheet.test.tsx
@@ -1,14 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react-native";
+import { render, wait } from "@testing-library/react-native";
 import sampleDoc from "../../../fixtures/demo-oc.json";
 import { DocumentDetailsSheet } from "./DocumentDetailsSheet";
 
+import { checkValidity } from "../../services/DocumentVerifier";
+jest.mock("../../services/DocumentVerifier");
+const checkValidityMock = checkValidity as jest.Mock;
+
+jest.useFakeTimers();
+
 describe("DocumentDetailsSheet", () => {
-  it("should show the correct issuer name", () => {
+  it("should show the correct issuer name", async () => {
     expect.assertions(1);
+    checkValidityMock.mockReturnValue([
+      Promise.resolve({
+        hash: { checksumMatch: true },
+        issued: { issuedOnAll: true },
+        revoked: { revokedOnAny: false },
+        valid: true
+      }),
+
+      Promise.resolve({
+        identifiedOnAll: true,
+        details: []
+      })
+    ]);
     const { queryByText } = render(
       <DocumentDetailsSheet document={sampleDoc} />
     );
-    expect(queryByText("Govtech")).not.toBeNull();
+    await wait(() => {
+      expect(queryByText("Govtech")).not.toBeNull();
+    });
   });
 });
