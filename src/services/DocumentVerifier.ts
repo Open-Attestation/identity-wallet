848b9d097dc6b731e8103cff109a2832557b6e73
benjaminlim00 <benjaminlim1996@hotmail.sg>
2020-01-30 11:15:49 +0800

    chore: use verify api (not working)

--- src/services/DocumentVerifier.ts
+++ src/services/DocumentVerifier.ts
@@ -1,42 +1,51 @@
-import { verifyWithIndividualChecks } from "@govtechsg/oa-verify";
-import { SignedDocument } from "@govtechsg/open-attestation";
-import { checkValidIdentity } from "./IdentityVerifier";
+import {
+  verificationBuilder,
+  openAttestationVerifiers
+} from "@govtechsg/oa-verify";
+import { v2, WrappedDocument } from "@govtechsg/open-attestation";
 
 // Ensures TS infers the type of the array as a tuple instead
 // https://github.com/Microsoft/TypeScript/pull/24897
-function tuple<T extends any[]>(...data: T): T {
-  return data;
-}
+// function tuple<T extends any[]>(...data: T): T {
+//   return data;
+// }
 
 // Let TS infer the return type
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-export const checkValidity = (
-  document: SignedDocument,
-  network = "ropsten" // TODO: handle this based on some user selection of the network
+export const checkValidity = async (
+  document: WrappedDocument<v2.OpenAttestationDocument>,
+  network = "ropsten", // TODO: handle this based on some user selection of the network,
+  callback
 ) => {
   const isMainnet = network === "mainnet";
   const networkName = isMainnet ? "homestead" : "ropsten";
 
-  const [verifyHash, verifyIssued, verifyRevoked] = verifyWithIndividualChecks(
-    document,
-    networkName
-  );
+  const verify = verificationBuilder(openAttestationVerifiers);
 
-  const verifyIdentity = checkValidIdentity(document, networkName);
+  const [
+    verifyHash,
+    ,
+    verifyIssued,
+    verifyRevoked,
+    verifyIdentity
+  ] = await verify(document, {
+    network: networkName,
+    promisesCallback: callback
+  });
 
   // If any of the checks are invalid, resolve the overall validity early
   const overallValidityCheck = Promise.all([
     new Promise(async (resolve, reject) =>
-      (await verifyHash).checksumMatch ? resolve() : reject()
+      (await verifyHash).verify ? resolve() : reject()
     ),
     new Promise(async (resolve, reject) =>
-      (await verifyIssued).issuedOnAll ? resolve() : reject()
+      (await verifyIssued).verify ? resolve() : reject()
     ),
     new Promise(async (resolve, reject) =>
-      (await verifyRevoked).revokedOnAny ? reject() : resolve()
+      (await verifyRevoked).verify ? reject() : resolve()
     ),
     new Promise(async (resolve, reject) =>
-      (await verifyIdentity).identifiedOnAll ? resolve() : reject()
+      (await verifyIdentity).verify ? resolve() : reject()
     )
   ])
     .then(() => true)
