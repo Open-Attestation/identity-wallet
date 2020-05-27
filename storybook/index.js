57805bb8fa497b4a43bd505ffd76b64ce7b5333d
Justus Soh <45728710+justussoh@users.noreply.github.com>
2020-05-27 13:45:38 +0800

    Feat/font loader (#85)
    * Added FontLoader
    
    * Linting changes

--- storybook/index.js
+++ storybook/index.js
@@ -1,4 +1,6 @@
-import { getStorybookUI, configure } from "@storybook/react-native";
+import React from "react";
+import { getStorybookUI, configure, addDecorator } from "@storybook/react-native";
+import { FontLoader } from "../src/components/FontLoader";
 
 import "./rn-addons";
 import "./config";
@@ -10,6 +12,8 @@ configure(() => {
 
 // Refer to https://github.com/storybookjs/storybook/tree/master/app/react-native#start-command-parameters
 // To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({});
+const StorybookUIRoot = getStorybookUI({ asyncStorage: null });
+
+addDecorator(storyFn => <FontLoader>{storyFn()}</FontLoader>);
 
 export default StorybookUIRoot;
