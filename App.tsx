53794e456de47fbb78840f2fd55fc40bd50feb5a
justussoh <justus.soh@hotmail.com>
2020-05-28 16:10:15 +0800

    Fix Buffer Variable not found Bug

--- App.tsx
+++ App.tsx
@@ -1,3 +1,7 @@
+// Can't find variable error
+// https://github.com/stockulus/pouchdb-react-native/issues/109
+global.Buffer = global.Buffer || require("buffer").Buffer;
+
 import AppNavigation from "./src/navigation";
 import Storybook from "./storybook";
 import { IS_STORYBOOK_VIEW } from "./src/config";
